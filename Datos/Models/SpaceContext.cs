7d3e4afb2e52ef5e8a2c8ab69ba7c57d1878d91f
Sebastián Gómez Valencia <98537540+sgomez1205@users.noreply.github.com>
2024-11-11 11:12:07 -0500

    Space Context Done

--- Datos/Models/SpaceContext.cs
+++ Datos/Models/SpaceContext.cs
@@ -4,6 +4,7 @@ namespace Datos.Models;
 
 public class SpaceContext : DbContext
 {
+    public static readonly string connectionString = "Server=localhost; User ID=root; Password=31895521Se.; Database=entertainment_spaces";
     public SpaceContext(DbContextOptions<SpaceContext> options) 
     : base(options)
     {
@@ -11,4 +12,9 @@ public class SpaceContext : DbContext
     }
 
     public DbSet<Space> Spaces {get; set;} = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 }
