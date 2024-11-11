5ad4218d542168b7c54c7dfa00ded6418d5c094f
Sebastián Gómez Valencia <98537540+sgomez1205@users.noreply.github.com>
2024-11-11 11:43:52 -0500

    Archivos Configuracuion Datos

--- Datos/Models/ZonesContext.cs
+++ Datos/Models/ZonesContext.cs
@@ -1,17 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using Entidades.Space;
+using Entidades.Zone;
 namespace Datos.Models;
 
-public class SpaceContext : DbContext
+public class ZonesContext : DbContext
 {
     public static readonly string connectionString = "Server=localhost; User ID=root; Password=31895521Se.; Database=entertainment_spaces";
-    public SpaceContext(DbContextOptions<SpaceContext> options) 
+    public ZonesContext(DbContextOptions<ZonesContext> options) 
     : base(options)
     {
 
     }
 
-    public DbSet<Space> Spaces {get; set;} = null!;
+    public DbSet<Zone> zones {get; set;} = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
